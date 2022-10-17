f7192aee69a82d3d46d986720a8680a0dfb20ec3
MLjoaolaureano <joao.laureano@mercadolivre.com>
2022-10-17 11:09:34 -0300

    docs:Criacao de JavaDoc para as Controller

--- src/main/java/com/desafios/desafio_quality/controller/RoomController.java
+++ src/main/java/com/desafios/desafio_quality/controller/RoomController.java
@@ -1,15 +1,20 @@
 package com.desafios.desafio_quality.controller;
 
 import com.desafios.desafio_quality.controller.dto.RoomAreaResponse;
-import com.desafios.desafio_quality.entity.Room;
-import com.desafios.desafio_quality.repository.RoomRepository;
 import com.desafios.desafio_quality.service.RoomService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * All endpoints related to Room
+ * It is a Spring @RestController
+ */
 @RestController
 @RequestMapping("/room")
 public class RoomController {
@@ -20,13 +25,25 @@ public class RoomController {
         this.roomService = roomService;
     }
 
+    /**
+     * Endpoint to return all the rooms and it respective area
+     * Returns 200 OK in case operation is success.
+     * @param id the property ID
+     * @return a list with rooms and it respective area
+     */
     @GetMapping("/{id}")
     ResponseEntity<List<RoomAreaResponse>> getAllAreas(@PathVariable Long id) {
         return ResponseEntity.ok(this.roomService.getAllRoomAreasByPropertyId(id));
     }
 
+    /**
+     * Endpoint to return the biggest room in a Property reference
+     * Returns 200 OK in case operation is success.
+     * @param id the property ID
+     * @return the biggest room in a property
+     */
     @GetMapping("/filter-bigger-room/{id}")
-    Optional<RoomAreaResponse> findBiggerRoom(@PathVariable Long id) {
-        return roomService.findBiggerRoom(id);
+    ResponseEntity<Optional<RoomAreaResponse>> findBiggerRoom(@PathVariable Long id) {
+        return ResponseEntity.ok(roomService.findBiggerRoom(id));
     }
 }
